c7890bea029af71059c9ce8062d47c209b593137
sam_wlh_ds <sam.wlh.ds@gmail.com>
2025-07-28 10:21:14 +0530

    Added Url fetching from Reddis Cache

--- server/db/dbQuery.js
+++ server/db/dbQuery.js
@@ -1,16 +1,34 @@
 import UrlModel from "../models/UrlModel.js";
+import { redisClient } from "./dbConnect.js";
 
 async function getLongUrl(shortUrl) {
-  try {
-    const url = await UrlModel.findOne({shortUrl});
-    if (!url) {
-      throw new Error("Url not found");
+    try {
+        const cacheUrl = await redisClient.get(shortUrl);
+        console.log(shortUrl, cacheUrl);
+        if (cacheUrl) {
+            try {
+                return JSON.parse(cacheUrl);
+            } catch (parseError) {
+                console.warn(
+                    `Redis: Failed to parse JSON from cache for ${shortUrl}. Error: ${parseError.message}. Falling back to MongoDB.`
+                );
+            }
+        }
+    } catch (redisError) {
+        console.error(
+            `Redis Error for ${shortUrl}: ${redisError.message}. Falling back to MongoDB.`
+        );
     }
-    return url;
-  } catch (error) {
-    throw new Error(error.message || "Failed to fetch url");
-  }
+
+    try {
+        const url = await UrlModel.findOne({ shortUrl });
+        if (!url) {
+            throw new Error("Url not found");
+        }
+        return url;
+    } catch (mongoDBError) {
+        throw new Error(mongoDBError.message || "Failed to fetch url");
+    }
 }
 
-
-export {getLongUrl};
+export { getLongUrl };
